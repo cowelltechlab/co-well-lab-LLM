98a4707ed99f7b865bbf8894760d293c62066c4f
Max Ohsawa <max.ohsawa@gmail.com>
2025-02-17 12:30:20 -0500

    adds modularized api test route

--- express/server.js
+++ express/server.js
@@ -2,33 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+// express
 const PORT = process.env.PORT || 3000
-
-const mongooseConnection = require('./db/connection');
-
 const app = express()
 
-app.get('/api/test-db', async (req, res) => {
-  // Check if the native connection is ready
-  if (!mongoose.connection.db) {
-    return res.status(500).json({ error: 'MongoDB connection not ready' });
-  }
-  
-  try {
-    const collections = await mongoose.connection.db.listCollections().toArray();
-    res.json({
-      message: 'MongoDB connection successful!',
-      collections,
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Error accessing MongoDB', details: error.message });
-  }
-});
+// routes
+const router = require('./routes')
+app.use('/', router)
+
+// db
+const mongooseConnection = require('./db/connection')
 
 mongooseConnection.once('open', () => {
-  console.log(`Mongoose connection open`);
+  console.log(`Mongoose connection open`)
   app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}`)
   })
-
 })
