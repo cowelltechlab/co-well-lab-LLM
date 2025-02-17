631afc0427e469b062eb330911f08eb0d9e0f5a2
Max Ohsawa <max.ohsawa@gmail.com>
2025-02-17 12:46:33 -0500

    adds proxy server configuration to vite so that requests made to /api endpoints will be routed to express running on port 3000

--- express/server.js
+++ express/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
@@ -6,9 +7,22 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3000
 const app = express()
 
+// global middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 // routes
 const router = require('./routes')
 app.use('/', router)
+
+// vite-react in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'vite-react', 'dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'vite-react', 'dist', 'index.html'))
+  })
+}
 
 // db
 const mongooseConnection = require('./db/connection')
