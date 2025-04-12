c5de175da3ff37b35c54a28169405c0bdb8ba904
Max Ohsawa <max.ohsawa@gmail.com>
2025-04-12 10:57:02 -0400

    refactors file structure and functions for new design, adds function headers for structure

---
+++ flask/routes/letter_lab.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, request, jsonify
+
+from app.services.openai_service import generate_initial_cover_letter
+from app.services.openai_service import generate_review_all_view_intro
+from app.services.pipeline_service import generate_bullet_points_and_rationales
+from app.services.mongodb_service import save_initialization
+
+letter_lab_bp = Blueprint("letter_lab", __name__)
+
+@letter_lab_bp.route('/initialize', methods=['POST'])
+def initialize():
+    try:
+        data = request.get_json()
+        resume = data.get("resume_text")
+        job_desc = data.get("job_desc")
+
+        if not resume or not job_desc:
+            return jsonify({"error": "Missing resume or job description"}), 400
+
+        # Task 1
+        initial_cover_letter = generate_initial_cover_letter(resume, job_desc)
+
+        # Task 2
+        review_all_view_intro = generate_review_all_view_intro(job_desc)
+
+        # Task 3 & 4
+        bullet_points, rationales = generate_bullet_points_and_rationales(resume, job_desc)
+
+        # Task 5
+        document_id = save_initialization(resume, job_desc, initial_cover_letter, review_all_view_intro, bullet_points, rationales)
+
+
+        return jsonify({
+            "initial_cover_letter": initial_cover_letter,
+            "review_all_view_intro": review_all_view_intro,
+            "bullet_points": bullet_points,
+            "rationales": rationales,
+            "document_id": document_id
+        })
+    except Exception as e:
+        print("Error processing cover letter:", str(e))
+        return jsonify({"error": "Error processing cover letter"}), 500
+
+@letter_lab_bp.route('/finalize', methods=['POST'])
+def finalize():
+    return
--- flask/services/mongodb_service.py
+++ flask/services/mongodb_service.py
@@ -1,11 +1,14 @@
 
 
-def save_user_request(resume_text, job_desc, cover_letter):
+def save_initialization(resume, job_desc, initial_cover_letter, review_all_view_intro, bullet_points, rationales):
     try:
         mongo.db.user_requests.insert_one({
-            "resume_text": resume_text,
+            "resume": resume,
             "job_desc": job_desc,
-            "cover_letter": cover_letter
+            "initial_cover_letter": initial_cover_letter,
+            "review_all_view_intro": review_all_view_intro,
+            "bullet_points": bullet_points,
+            "rationales": rationales
         })
     except Exception as e:
         print(f"MongoDB Error: {str(e)}")
